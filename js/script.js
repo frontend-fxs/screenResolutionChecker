f46da5e8fd0927e1650bffec67219dbeb1089600
frontend-fxs <oriol@fxstreet.com>
2019-02-22 16:51:59 +0100

    up

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+
+document.querySelector('#width').innerHTML = width;
+document.querySelector('#height').innerHTML = height;
