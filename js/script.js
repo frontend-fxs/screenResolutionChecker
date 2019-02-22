0a39e247d7b65e02dc9cf372c18a89a038a4e1a5
frontend-fxs <oriol@fxstreet.com>
2019-02-22 17:06:15 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+var devicePixelRatio = window.devicePixelRatio;
 
 document.querySelector('#width').innerHTML = width;
 document.querySelector('#height').innerHTML = height;
+document.querySelector('#devicePixelRatio').innerHTML = devicePixelRatio;
